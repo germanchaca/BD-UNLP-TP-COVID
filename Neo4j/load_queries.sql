d08fcc1965ef5bfe0464fbb1d5d7e581e0ab4704
germanchaca <germanchaca@gmail.com>
2020-05-10 19:55:20 -0300

    MOngo

---
+++ Neo4j/load_queries.sql
@@ -0,0 +1,39 @@
+:auto USING PERIODIC COMMIT 1000
+LOAD CSV FROM "file:///friends-000______.txt" as line FIELDTERMINATOR ":"
+MERGE (u1:User {id:line[0]});
+
+:auto USING PERIODIC COMMIT 1000
+LOAD CSV FROM "file:///friends-000______.txt" as line FIELDTERMINATOR ":"
+WITH line[1] as id2
+WHERE id2 <> '' AND id2 <> 'private' AND id2 <> 'notfound'
+UNWIND split(id2,",") as id
+WITH distinct id
+MERGE (:User {id:id});
+
+:auto USING PERIODIC COMMIT 1000
+LOAD CSV FROM "file:///friends-000______.txt" as line FIELDTERMINATOR ":"
+WITH line[0] as id1, line[1] as id2
+WHERE id2 <> '' AND id2 <> 'private' AND id2 <> 'notfound'
+MATCH (u1:User {id:id1})
+UNWIND split(id2,",") as id 
+MATCH (u2:User {id:id})
+CREATE (u1)-[:FRIEND_OF]-(u2);
+
+
+MATCH (n)
+RETURN count(n) as count_n;
+
+MATCH ()-[r]-()
+RETURN count(r) as count_r;
+
+
+
+:auto USING PERIODIC COMMIT 1000
+LOAD CSV FROM "file:///friends-000______.txt" as line FIELDTERMINATOR ":"
+MERGE (u1:User {id:line[0]})
+WITH line[1] as id2
+WHERE id2 <> '' AND id2 <> 'private' AND id2 <> 'notfound'
+UNWIND split(id2,",") as id
+WITH distinct id
+MERGE (u2:User {id:id})
+CREATE (u1)-[:FRIEND_OF]->(u2);
